0e0999d9674e94ddaf40fe4458e27368eb248e08
Piotr Zoladz <zoladzp@ads.autodesk.com>
2016-06-03 13:16:26 +0200

    Enabling Ec2SetPassword and Ec2HandleUserData http://docs.aws.amazon.com/AWSEC2/latest/WindowsGuide/windows-amis-guidelines.html

---
+++ EnableEC2config.ps1
@@ -0,0 +1,12 @@
+﻿$path = 'C:\Program Files\Amazon\Ec2ConfigService\Settings\config.xml'
+$StateValue ='Enabled'
+
+
+$xml = [xml](Get-Content $path)
+$node = $xml.Ec2ConfigurationSettings.Plugins.Plugin | where {$_.Name -eq 'Ec2SetPassword'}
+$node.State = $StateValue
+
+$node = $xml.Ec2ConfigurationSettings.Plugins.Plugin | where {$_.Name -eq 'Ec2HandleUserData'}
+$node.State = $StateValue
+
+$xml.Save($path)
